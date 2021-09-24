4b01a324421317c47187a84d031bca07d62d56d0
angelinalis <angelina.lis18@gmail.com>
2021-09-24 20:37:57 +0300

    added likes add login and logout annotation SQL and select related

--- sales_manager/models.py
+++ sales_manager/models.py
@@ -4,8 +4,9 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=124, db_index=True, verbose_name='Название')
     text = models.TextField(verbose_name='Описание', help_text='Введите описание книги')
-    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор', related_name='books')
     date_publish = models.DateField(auto_now_add=True, db_index=True)
+    likes = models.ManyToManyField(User, related_name="liked_books")
     def __str__(self):
         return self.title
 
