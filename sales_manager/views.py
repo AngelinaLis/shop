901b0efb9edabb4fcc85ab0a6b99ba17c9b63124
angelinalis <angelina.lis18@gmail.com>
2021-09-27 16:32:49 +0300

    work with template extensions and template include

--- sales_manager/views.py
+++ sales_manager/views.py
@@ -19,13 +19,16 @@ def book_detail(request, book_id):
     return render(request, "sales_manager/book_detail.html", context)
 
 @login_required()
-def book_like(request, book_id):
+def book_like(request, book_id, redirect_url):
     book = Book.objects.get(id=book_id)
     if request.user in book.likes.all():
         book.likes.remove(request.user)
     else:
         book.likes.add(request.user)
-    return redirect("main-page")
+    if redirect_url == "main-page":
+        return redirect("main-page")
+    elif redirect_url == "book-detail":
+        return redirect("book-detail", book_id=book_id)
 
 class LoginView(View):
     def get(self, request):
